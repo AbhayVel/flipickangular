5ffabf59ed8f864dbc42ee8c09f9ef0f046d8b85
abhayvel <quickitdotnet@gmail.com>
2022-05-28 09:26:18 +0530

    added util

--- argon-dashboard-angular-master/argon-dashboard-angular-master/src/app/utilities/utility.ts
+++ argon-dashboard-angular-master/argon-dashboard-angular-master/src/app/utilities/utility.ts
@@ -13,7 +13,10 @@ export function sort(list: Array<any>, columnName: string, orderBy: number , col
       return a[columnName] > b[columnName] ? -1 * orderBy : 1 * orderBy;
     } else if (columnType == 'ci') {    //
       return a[columnName].toLowerCase() > b[columnName].toLowerCase() ? -1 * orderBy : 1 * orderBy;
-    } else {
+    } else if (columnType == 'date') {
+
+    }
+    else {
       return a[columnName] > b[columnName] ? -1 * orderBy : 1 * orderBy;
     }
 });

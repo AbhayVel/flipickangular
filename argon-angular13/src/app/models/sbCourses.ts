457cc3c8eafc6e8d3078f121992b7e5ad4470dd7
Shekhar <89627255+ShekharB703@users.noreply.github.com>
2022-06-09 08:36:07 +0530

    Shekhar

---
+++ argon-angular13/src/app/models/sbCourses.ts
@@ -0,0 +1,9 @@
+export class sbCourses {
+    id: number = 0;
+    icon: string = '';
+    name: string = '';
+    numberOfStudents: number = 0;
+    status: number = 0;
+    authors: string = '';
+    duration: number = 0;
+}
